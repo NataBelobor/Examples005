9ef20b57e29d459ef0e16734d530dbf8c712ce30
Nata <Beloborodova.n.s@bk.ru>
2022-07-17 05:49:05 +0300

    dz13

---
+++ dz13/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 13: Напишите программу, которая выводит 
+//третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int n;
+Console.WriteLine("Введите число: ");
+n = Convert.ToInt32(Console.ReadLine());
+if (n<=99)
+{
+    Console.WriteLine("третьей цифры нет");
+} 
+if (n>99)
+{
+    String str = n.ToString ();
+   Console.WriteLine(str[2]); 
+}
